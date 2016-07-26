027a85e135f2910b8db951ee24ead8f18d76cccf
Troy Chryssos <github email address>
2016-07-26 15:59:59 -0400

    Updated methods to pull data from swapi and return the movies a given character is in.

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,26 +1,60 @@
 require 'rest-client'
+require 'pry'
+require 'JSON'
 
 def get_character_movies_from_api(character)
-  #make the web request
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  # iterate ove the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film. 
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list 
-  #  of movies by title. play around with puts out other info about a given film.
+
+  character_films=[]
+
+  character_hash["results"].each do |person|
+    # binding.pry
+    if person["name"].downcase==character
+      character_films=person["films"]
+    end
+  end
+
+  character_films.collect do |film|
+    current_film=RestClient.get("#{film}")
+    JSON.parse(current_film)
+  end
 end
 
 def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  films_hash.each do |film|
+    puts film["title"]
+  end
 end
+
+# def parse_character_movies(films_hash)
+#   sw_movies=[]
+#   films_hash.each do |film|
+#     sw_movies<<film["title"]
+#     sw_movies.sort_by do |movies|
+#       film["release_date"][0..4].to_i
+#     end
+#   puts sw_movies
+#   end
+
+  # sw_movies=sw_movies.sort_by do |movie|
+  #   sw_hash={"A New Hope"=>1977,
+  #   "Empire Strikes Back"=>1980,
+  #   "Return of the Jedi"=>1983,
+  #   "The Phantom Menace"=>1999,
+  #   "Attack of the Clones"=>2002,
+  #   "Revenge of the Sith"=>2005,
+  #   "The Force Awakens"=>2016}
+  # end
+
+  # puts sw_movies
+
+
+
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
+  # binding.pry
   parse_character_movies(films_hash)
 end
 
